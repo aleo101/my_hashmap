fac19782fdb51db1fdbbb5309394f8e1b81e5fff
aleo101 <55769947+aleo101@users.noreply.github.com>
2021-08-17 18:24:01 -0700

    removed all smart pointers as they were unneeded.

--- src/main.rs
+++ src/main.rs
@@ -10,32 +10,32 @@ const MAP_OMEM: i16 = -1;
 const MAP_OK: i16 = 0;
 const INIT_CAP: usize = 1024;
 
-type MapT<T> = Box<HashMapMap<T>>;
+type MapT<T> = HashMapMap<T>;
 
 #[repr(C)]
 #[derive(Default, Clone)]
 struct HashMapElement<T> {
     key: i32,
     in_use: i32,
-    data: Box<T>,
+    data: T,
 }
 
 #[repr(C)]
 struct HashMapMap<T> {
     table_size: usize,
     size: i32,
-    data: Box<Vec<HashMapElement<T>>>,
+    data: Vec<HashMapElement<T>>,
 }
 
 fn hashmap_new<T>() -> MapT<T>
 where
     T: Clone + Default,
 {
-    let m = Box::new(HashMapMap {
+    let m = HashMapMap {
         table_size: INIT_CAP,
         size: 0,
-        data: Box::new(vec![HashMapElement::<T>::default(); INIT_CAP]),
-    });
+        data: vec![HashMapElement::<T>::default(); INIT_CAP],
+    };
 
     m
 }
@@ -60,20 +60,20 @@ fn hashmap_hash_int<T>(m: &HashMapMap<T>, mut key: usize) -> usize {
 use std::convert::TryInto;
 use std::mem;
 
-fn hashmap_hash<T>(inside: &MapT<T>, key: i32) -> i16 {
-    if inside.size == inside.table_size.try_into().unwrap() {
+fn hashmap_hash<T>(m: &MapT<T>, key: i32) -> i16 {
+    if m.size == m.table_size.try_into().unwrap() {
         return MAP_FULL;
     }
-    let mut curr: usize = hashmap_hash_int(&inside, key.try_into().unwrap());
-    for _ in 0..inside.table_size {
-        if inside.data[curr].in_use == 0 {
+    let mut curr: usize = hashmap_hash_int(&m, key.try_into().unwrap());
+    for _ in 0..m.table_size {
+        if m.data[curr].in_use == 0 {
             return curr as i16;
         }
-        if inside.data[curr].key == key && inside.data[curr].in_use == 1 {
+        if m.data[curr].key == key && m.data[curr].in_use == 1 {
             return curr as i16;
         }
 
-        curr = curr + 1 % inside.table_size
+        curr = curr + 1 % m.table_size
     }
 
     MAP_FULL
@@ -83,7 +83,7 @@ fn hashmap_rehash<T>(m: &mut MapT<T>) -> i16
 where
     T: Default + Copy,
 {
-    let mut curr = Box::new(vec![HashMapElement::<T>::default(); 2 * INIT_CAP]);
+    let mut curr = vec![HashMapElement::<T>::default(); 2 * INIT_CAP];
     // let curr point to old data in memory
     //let data field of m now point to new default-init'd vector.
     mem::swap(&mut m.data, &mut curr);
@@ -92,7 +92,7 @@ where
     m.size = 0;
 
     for i in 0..old_size {
-        let status: i16 = hashmap_put(m, curr[i].key, &curr[i].data);
+        let status: i16 = hashmap_put(m, curr[i].key, curr[i].data);
         if status != MAP_OK {
             return status;
         }
@@ -101,7 +101,7 @@ where
     return MAP_OK;
 }
 
-fn hashmap_put<T>(m: &mut MapT<T>, key: i32, value: &Box<T>) -> i16
+fn hashmap_put<T>(m: &mut MapT<T>, key: i32, value: T) -> i16
 where
     T: Clone + Default + Copy,
 {
@@ -126,7 +126,7 @@ where
     let mut curr = hashmap_hash_int(&m, key);
     for _ in 0..m.table_size {
         if m.data[curr].key == key as i32 && m.data[curr].in_use == 1 {
-            return Some(*m.data[curr].data);
+            return Some(m.data[curr].data);
         }
         curr = (curr + 1) % m.table_size;
     }
@@ -147,7 +147,7 @@ where
                 m.data[i].in_use = 0;
                 m.size -= 1;
             }
-            return Some(*m.data[i].data);
+            return Some(m.data[i].data);
         }
     }
     None
@@ -162,7 +162,7 @@ where
         if m.data[curr].key == key as i32 && m.data[curr].in_use == 1 {
             /* Blank out the fields */
             m.data[curr].in_use = 0;
-            m.data[curr].data = Box::default();
+            m.data[curr].data = T::default();
             m.data[curr].key = 0;
             /* Reduce the size */
             m.size -= 1;
@@ -178,8 +178,7 @@ fn hashmap_length<T>(m: &MapT<T>) -> i32 {
 }
 fn main() {
     let map = &mut hashmap_new::<i32>();
-    let i = Box::new(4);
-    hashmap_put(map, 1, &i);
+    hashmap_put(map, 1, 4);
     println!(
         "Getting random (first?) element: {}",
         hashmap_get_one(map, 0).unwrap()
@@ -188,8 +187,7 @@ fn main() {
         "Getting random (first?) element: {}",
         hashmap_get_one(map, 0).unwrap()
     );
-    let j = Box::new(365);
-    hashmap_put(map, 2, &j);
+    hashmap_put(map, 2, 365);
     println!(
         "Getting random (first?) element: {}",
         hashmap_get_one(map, 0).unwrap()
